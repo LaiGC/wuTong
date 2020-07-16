f062fb9b0b92d6a11413ffd571de8a2a2101e7cf
赖国财 <1521712189@163.com>
2020-07-16 19:54:59 +0800

    提交修改后的git

--- git_test/src/main/java/com/itheima/git/FirstClass.java
+++ git_test/src/main/java/com/itheima/git/FirstClass.java
@@ -4,6 +4,9 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git");
 
+        FirstClass a = new FirstClass();
+        int sum = a.add(1, 2);
+        System.out.println(sum);
     }
 
     public int add(int a,int b){
